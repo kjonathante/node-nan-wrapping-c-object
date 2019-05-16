5e2148fa54b0d6cb9599e05e3cf7c35f0665cf09
Kit Jonathan Te <kjonathante@gmail.com>
2019-05-15 20:40:22 -0700

    Added boilerplate code for wrapping c++ code

--- src/myobject.h
+++ src/myobject.h
@@ -1,1 +1,32 @@
 // myobject.h
+/*
+class MyObject : public node::ObjectWrap {
+ public:
+  static void Init(v8::Local<v8::Object> exports);
+
+ private:
+  explicit MyObject(double value = 0);
+  ~MyObject();
+
+  static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void PlusOne(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static v8::Persistent<v8::Function> constructor;
+  double value_;
+};
+*/
+
+#include <nan.h>
+
+class MyObject : public Nan::ObjectWrap {
+  public:
+    static NAN_MODULE_INIT(Init);
+
+  private:
+    explicit MyObject(double value = 0);
+    ~MyObject();
+
+    static NAN_METHOD(New);
+    static NAN_METHOD(PlusOne);
+    static Nan::Persistent<v8::Function> constructor;
+    double value_;
+};
